8f5daecb617ee0661e1301748942896698be5483
aristo <dkfud2121@naver.com>
2025-05-09 22:34:45 +0900

    section2 [JDBC 이해] JDBC 개발- 수정, 삭제

--- inflearn/springDB-part.1/jdbc/src/test/java/com/inflearn/kyh/jdbc/repository/MemberRepositoryVOTest.java
+++ inflearn/springDB-part.1/jdbc/src/test/java/com/inflearn/kyh/jdbc/repository/MemberRepositoryVOTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import com.inflearn.kyh.jdbc.domain.Member;
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -15,11 +16,19 @@ class MemberRepositoryVOTest {
 
   @Test
   void save() throws SQLException {
-    Member member = new Member("memberVO1", 10000);
+    Member member = new Member("memberVO10", 10000);
     repository.save(member);
 
     Member findMember = repository.findById(member.getMemberId());
     log.info("findMember={}", findMember);
     Assertions.assertThat(findMember).isEqualTo(member);
+
+    repository.update(member.getMemberId(), 20000);
+    Member updateMember = repository.findById(member.getMemberId());
+    Assertions.assertThat(updateMember.getMoney()).isEqualTo(20000);
+
+    repository.delete(member.getMemberId());
+    Assertions.assertThatThrownBy( () -> repository.findById(member.getMemberId()))
+        .isInstanceOf(NoSuchElementException.class);
   }
 }
