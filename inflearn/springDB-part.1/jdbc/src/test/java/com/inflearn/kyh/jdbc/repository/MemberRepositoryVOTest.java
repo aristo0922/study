0e5e2ca9c450ff19b67b9d433828ec9a6d982e9f
aristo <dkfud2121@naver.com>
2025-05-09 21:56:38 +0900

    feat [#2-JDBC이해] JDBC 개발 - 조회

--- inflearn/springDB-part.1/jdbc/src/test/java/com/inflearn/kyh/jdbc/repository/MemberRepositoryVOTest.java
+++ inflearn/springDB-part.1/jdbc/src/test/java/com/inflearn/kyh/jdbc/repository/MemberRepositoryVOTest.java
@@ -4,15 +4,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import com.inflearn.kyh.jdbc.domain.Member;
 import java.sql.SQLException;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+@Slf4j
 class MemberRepositoryVOTest {
 
   MemberRepositoryVO repository = new MemberRepositoryVO();
 
   @Test
   void save() throws SQLException {
-    Member member = new Member("memberVO", 10000);
+    Member member = new Member("memberVO1", 10000);
     repository.save(member);
+
+    Member findMember = repository.findById(member.getMemberId());
+    log.info("findMember={}", findMember);
+    Assertions.assertThat(findMember).isEqualTo(member);
   }
 }
